9512faebc8210d447e090db8ae760ffb00ed09d2
dp-rp <155602873+dp-rp@users.noreply.github.com>
2024-11-29 21:24:01 +1100

    move cob stuff into it's own file

---
+++ tasktrackerdesktopwidget/cob.py
@@ -0,0 +1,33 @@
+import os
+from datetime import datetime, time
+
+
+# TODO: pick a random message from a configurable JSON file that contains a big list of different messages to show
+# ... next to the COB timer when it's past COB. e.g. "Go home!", "Do some yoga!", "Drink some water!", "Water your
+# ... plants!", "Walk your dog!", "Pick up your kids!", "Go watch Netflix!", "Go nap!", "Go make some art!", "Go
+# ... call a friend!", "Geddoutta'ere!"
+PAST_COB_MESSAGE = "go home!"
+
+
+def get_time_until_cob_msg():
+    # TODO: HACK: would rather not read this from env var every time we call this func - good enough for now
+    cob_hour = int(os.environ['COB_TIMER_COB_HOUR'])
+    cob_minute = int(os.environ['COB_TIMER_COB_MINUTE'])
+
+    # Calculate the difference in time
+    cob = datetime.combine(datetime.now().date(), time(cob_hour, cob_minute))
+    time_difference = cob - datetime.now()
+
+    # If the current time is already past 5 PM, calculate for the next day
+    # if time_difference.total_seconds() < 0:
+    #     cob = cob + timedelta(days=1)
+    #     time_difference = cob - now
+
+    # Calculate the number of hours left
+    seconds_left = time_difference.total_seconds()
+    hours_left = f"{seconds_left / 3600:01.1f}"
+    text = f"There are {hours_left} hours left until close of business ({cob_hour:02}:{cob_minute:02})" + (' ---- ' + PAST_COB_MESSAGE) if seconds_left < 0 else ''
+    # HACK: newline is getting stripped out for some reason unless I do it this way
+    text += "\n"
+
+    return text
--- tasktrackerdesktopwidget/overlay.py
+++ tasktrackerdesktopwidget/overlay.py
@@ -3,8 +3,7 @@
 
 import tkinter as tk
 from .generic.workitems import get_generic_work_items
-from datetime import datetime, time
-import os
+from .cob import get_time_until_cob_msg
 
 # TODO: get the output (my in-progress work items) and make it display in a small movable, resizable window that's
 # ... always-on-top that updates once every 5-10 minutes (and it should also go invisible for a couple seconds when
@@ -13,33 +12,8 @@ import os
 # TODO: display a metric for [HOURS LOGGED SO FAR] vs [HOURS LOGGED GOAL]. [HOURS LOGGED GOAL] will be
 # ... `[DAILY_HOUR_LOG_GOAL] * [NUMBER_OF_DAYS_INTO_CURRENT_ITERATION]` (current iteration = current sprint)
 
-# TODO: pick a random message from a configurable JSON file that contains a big list of different messages to show
-# ... next to the COB timer when it's past COB. e.g. "Go home!", "Do some yoga!", "Drink some water!", "Water your
-# ... plants!", "Walk your dog!", "Pick up your kids!", "Go watch Netflix!", "Go nap!", "Go make some art!", "Go
-# ... call a friend!"
-
 # TODO: make this configurable and also something a little less frequent (a web request every 10 seconds is a bit overkill)
 POLL_INTERVAL_MS = 10000  # milliseconds
-PAST_COB_MESSAGE = "go home!"
-
-def get_time_until_cob_msg():
-    # TODO: HACK: would rather not read this from env var every time we call this func - good enough for now
-    cob_hour = int(os.environ['COB_TIMER_COB_HOUR'])
-    cob_minute = int(os.environ['COB_TIMER_COB_MINUTE'])
-
-    # Calculate the difference in time
-    five_pm = datetime.combine(datetime.now().date(), time(cob_hour, cob_minute))
-    time_difference = five_pm - datetime.now()
-
-    # If the current time is already past 5 PM, calculate for the next day
-    # if time_difference.total_seconds() < 0:
-    #     five_pm = five_pm + timedelta(days=1)
-    #     time_difference = five_pm - now
-
-    # Calculate the number of hours left
-    seconds_left = time_difference.total_seconds()
-    hours_left = f"{seconds_left / 3600:01.1f}"
-    return f"There are {hours_left} hours left until close of business ({cob_hour:02}:{cob_minute:02}){' ---- ' + PAST_COB_MESSAGE if seconds_left < 0 else ''}\n"
 
 
 class MovableOverlay:
