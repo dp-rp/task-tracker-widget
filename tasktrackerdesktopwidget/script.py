50d884bd8d4a49c417acea47fa0b0a77cb351332
dp-rp <155602873+dp-rp@users.noreply.github.com>
2024-11-29 11:40:34 +1100

    start refactoring to support different remote ticket systems

--- tasktrackerdesktopwidget/script.py
+++ tasktrackerdesktopwidget/script.py
@@ -3,33 +3,47 @@ from msrest.authentication import BasicAuthentication
 from azure.devops.v7_1.work_item_tracking.models import Wiql
 import pprint
 from dotenv import load_dotenv
+import os
+
+remote_type_envvar_key = "TTW_REMOTE_TYPE"
 
 def main():
     load_dotenv()  # take environment variables
 
-    # Create a connection to the org
-    credentials = BasicAuthentication('', personal_access_token)
-    connection = Connection(base_url=organization_url, creds=credentials)
+    _remote_type = os.getenv(remote_type_envvar_key)
 
-    # Get a client (the "core" client provides access to projects, teams, etc)
-    core_client = connection.clients.get_core_client()
-
-    #query workitems, custom field 'RTCID' has a certain specific unique value
-    work_item_tracking_client = connection.clients.get_work_item_tracking_client()
-    query  = "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.State] = 'In Progress' AND [System.AssignedTo] = @me"
-    #convert query str to wiql
-    wiql = Wiql(query=query)
-    query_results = work_item_tracking_client.query_by_wiql(wiql).work_items
-    #get the results via title
-    for item in query_results:
-        work_item = work_item_tracking_client.get_work_item(item.id)
-        print("-------")
-        # print(work_item.__str__())
-        # pprint.pprint(work_item.fields['System.Title'])
-        print(f"{work_item.id}: {work_item.fields['System.Title']}")
+    if _remote_type == "ado-work-item":
+        # TODO: schema validation
         
-        if 'System.AssignedTo' in work_item.fields:
-            print(work_item.fields['System.AssignedTo']['uniqueName'])
+        # get sensitive config
+        personal_access_token = os.getenv('ADO_PERSONAL_ACCESS_TOKEN')
+        organization_url = os.getenv('ADO_ORGANIZAION_URL')
 
-    # TODO: get the output (my in-progress work items) and make it display in a small movable, resizable window that's always-on-top that updates once every 5-10 minutes
-    # ... (and it should also go invisible for a couple seconds when moused over, just like how I have ElevenClock configured)
+        # Create a connection to the org
+        credentials = BasicAuthentication('', personal_access_token)
+        connection = Connection(base_url=organization_url, creds=credentials)
+
+        # Get a client (the "core" client provides access to projects, teams, etc)
+        core_client = connection.clients.get_core_client()
+
+        #query workitems, custom field 'RTCID' has a certain specific unique value
+        work_item_tracking_client = connection.clients.get_work_item_tracking_client()
+        query  = "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.State] = 'In Progress' AND [System.AssignedTo] = @me"
+        #convert query str to wiql
+        wiql = Wiql(query=query)
+        query_results = work_item_tracking_client.query_by_wiql(wiql).work_items
+        #get the results via title
+        for item in query_results:
+            work_item = work_item_tracking_client.get_work_item(item.id)
+            print("-------")
+            # print(work_item.__str__())
+            # pprint.pprint(work_item.fields['System.Title'])
+            print(f"{work_item.id}: {work_item.fields['System.Title']}")
+            
+            if 'System.AssignedTo' in work_item.fields:
+                print(work_item.fields['System.AssignedTo']['uniqueName'])
+
+        # TODO: get the output (my in-progress work items) and make it display in a small movable, resizable window that's always-on-top that updates once every 5-10 minutes
+        # ... (and it should also go invisible for a couple seconds when moused over, just like how I have ElevenClock configured)
+    else:
+        raise Exception(f"{remote_type_envvar_key} isn't a supported value")
