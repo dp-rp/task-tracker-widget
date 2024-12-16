e470e54b9b6652a9b67569c7e59fb027b6358a5a
dp-rp <155602873+dp-rp@users.noreply.github.com>
2024-12-16 12:04:49 +1100

    add work item web browser link to generic ado work item properties so that links can be added later

--- tasktrackerdesktopwidget/generic/workitems.py
+++ tasktrackerdesktopwidget/generic/workitems.py
@@ -1,20 +1,27 @@
 import os
 from ..ado.workitems import get_ado_work_items
+import urllib
 
 REMOTE_TYPE_ENVVAR_KEY = "TTW_REMOTE_TYPE"
+
+def get_ado_generic(ado_work_item):
+    team_name_url_encoded = urllib.parse.quote(os.environ['ADO_TEAM_NAME'])
+    iteration_path_for_url = ado_work_item.fields['System.IterationPath'].replace('\\','/')
+    return {
+        "uid": ado_work_item.id,
+        "title": ado_work_item.fields['System.Title'],
+        "assigned_to": {
+            "unique_name": ado_work_item.fields['System.AssignedTo']['uniqueName']
+        },
+        "work_item_web_browser_link": f"https://dev.azure.com/recordpoint/{ado_work_item.fields['System.TeamProject']}/_sprints/backlog/{team_name_url_encoded}/{iteration_path_for_url}?workitem={ado_work_item.id}"
+    }
 
 def get_generic_work_items():
     _remote_type = os.getenv(REMOTE_TYPE_ENVVAR_KEY)
 
     if _remote_type == "ado-work-item":
         return [
-            {
-                "uid": ado_work_item.id,
-                "title": ado_work_item.fields['System.Title'],
-                "assigned_to": {
-                    "unique_name": ado_work_item.fields['System.AssignedTo']['uniqueName']
-                }
-            }
+            get_ado_generic(ado_work_item)
             for ado_work_item
             in get_ado_work_items()
         ]
