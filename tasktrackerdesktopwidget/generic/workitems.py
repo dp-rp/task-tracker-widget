cdad2996537235748b9996d3a9f707c0c22a6201
dp-rp <155602873+dp-rp@users.noreply.github.com>
2024-12-18 20:07:24 +1100

    get org url from env var when setting url for work item web browser link

--- tasktrackerdesktopwidget/generic/workitems.py
+++ tasktrackerdesktopwidget/generic/workitems.py
@@ -7,13 +7,14 @@ REMOTE_TYPE_ENVVAR_KEY = "TTW_REMOTE_TYPE"
 def get_ado_generic(ado_work_item):
     team_name_url_encoded = urllib.parse.quote(os.environ['ADO_TEAM_NAME'])
     iteration_path_for_url = ado_work_item.fields['System.IterationPath'].replace('\\','/')
+    org_url = os.environ['ADO_ORGANIZATION_URL']
     return {
         "uid": ado_work_item.id,
         "title": ado_work_item.fields['System.Title'],
         "assigned_to": {
             "unique_name": ado_work_item.fields['System.AssignedTo']['uniqueName']
         },
-        "work_item_web_browser_link": f"https://dev.azure.com/recordpoint/{ado_work_item.fields['System.TeamProject']}/_sprints/backlog/{team_name_url_encoded}/{iteration_path_for_url}?workitem={ado_work_item.id}"
+        "work_item_web_browser_link": f"{org_url}/{ado_work_item.fields['System.TeamProject']}/_sprints/backlog/{team_name_url_encoded}/{iteration_path_for_url}?workitem={ado_work_item.id}"
     }
 
 def get_generic_work_items():
